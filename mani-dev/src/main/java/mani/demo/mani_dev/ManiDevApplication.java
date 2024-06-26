78d002ce6135bdad5933a07a97eb05872c19c61c
mani <mani@gmail.com>
2024-06-26 17:24:07 +0530

    Initial commit

---
+++ mani-dev/src/main/java/mani/demo/mani_dev/ManiDevApplication.java
@@ -0,0 +1,13 @@
+package mani.demo.mani_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManiDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManiDevApplication.class, args);
+	}
+
+}
---
+++ mani-dev/src/test/java/mani/demo/mani_dev/ManiDevApplicationTests.java
@@ -0,0 +1,13 @@
+package mani.demo.mani_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManiDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
